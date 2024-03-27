d8d7f6d17a6711bacc93c981035eb718e786b535
Дмитрий Сенцов <kassen.ds@gmail.com>
2024-03-27 21:40:13 +0300

    Оповещение при простой регистрации.

--- src/log/devNotification.ts
+++ src/log/devNotification.ts
@@ -7,8 +7,21 @@ export async function registerNotification(ctx: Context, user_id: number) {
   const student = await findStudentById(user_id)
   if (student) {
     console.log(student)
-
-    const notifyText = `Зарегестрирован ученик: ${Object.keys(student)}`
+    const notifyText =
+      `Зарегестрирован ученик: ${student.name} ${student.surname}:\n` +
+      `user_id: ${student.user_id} \n`
     ctx.api.sendMessage(ctx.config.botDeveloper, notifyText)
   }
 }
+
+// {
+//   _id: new ObjectId('66040acd4c654ec96b222f74'),
+//   user_id: 809673990,
+//   invitation_code: 'd0648366-3d02-4ed4-bbcf-d3691ea86133',
+//   surname: 'фа',
+//   name: 'им',
+//   __v: 0,
+//   createdAt: 2024-03-27T12:02:21.210Z,
+//   parent_id: [],
+//   updatedAt: 2024-03-27T12:02:21.210Z
+// }
